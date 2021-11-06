3e389bf3f12c1a28a7bd5f4af2a85a95f8a82090
Denilson <denilson_contato@outlook.com>
2021-11-06 12:17:45 -0300

    Change name and creation models Board, Field

---
+++ src/br/com/dhentech/cm/model/Board.java
@@ -0,0 +1,5 @@
+package br.com.dhentech.cm.model;
+
+public class Board {
+
+}
---
+++ test/br/com/dhentech/cm/Test.java
@@ -0,0 +1,22 @@
+package br.com.dhentech.cm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void testEqualsTwo() {
+		
+		int a = 1 + 1;
+		
+		assertEquals(2, a);
+	}
+	
+	@org.junit.jupiter.api.Test
+	void TestingIfEqualsThree() {
+		int x = 2 + 10 - 9;
+		
+		assertEquals(3, x);
+	}
+
+}
