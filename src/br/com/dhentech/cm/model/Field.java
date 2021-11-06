d6191cf01661bcfa763cfdd248cb1318818dea11
Denilson <denilson_contato@outlook.com>
2021-11-06 12:54:30 -0300

    Implements attributes in model Field

--- src/br/com/dhentech/cm/model/Field.java
+++ src/br/com/dhentech/cm/model/Field.java
@@ -1,5 +1,23 @@
 package br.com.dhentech.cm.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Field {
+
+	private final Integer line;
+	private final Integer column;
+
+	private Integer idField;
+	private boolean mined = false;
+	private boolean open = false;
+	private boolean marked = false;
+
+	private List<Field> neighborhoods = new ArrayList<Field>();
+
+	public Field(Integer line, Integer column) {
+		this.line = line;
+		this.column = column;
+	}
 
 }
