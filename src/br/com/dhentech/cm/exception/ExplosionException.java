b340f242c6206c5b4309d230b987ca1f3ad1dc87
Denilson <denilson_contato@outlook.com>
2021-11-08 09:03:47 -0300

    Implemented method openField and safeneighborhood at model field

---
+++ src/br/com/dhentech/cm/exception/ExplosionException.java
@@ -0,0 +1,8 @@
+package br.com.dhentech.cm.exception;
+
+public class ExplosionException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
--- src/br/com/dhentech/cm/model/Field.java
+++ src/br/com/dhentech/cm/model/Field.java
@@ -3,6 +3,8 @@ package br.com.dhentech.cm.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.dhentech.cm.exception.ExplosionException;
+
 public class Field {
 
 	private final Integer line;
@@ -38,6 +40,45 @@ public class Field {
 		}
 
 		return false;
+	}
+
+	/*
+	 * Alterar a marks quando o camopo estiver fechado Change tag when field is
+	 * closed
+	 */
+	void changeMarking() {
+		if (!open) {
+			marked = !marked;
+		}
+	}
+
+	boolean openField() {
+		if (!open && !marked) {
+			open = true;
+
+			if (mined) {
+				throw new ExplosionException();
+			}
+
+			if (safeneighborhood()) {
+				neighborhoods.forEach(v -> v.openField());
+			}
+
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * Garante que quando clica em um campo, os campos ao lado sejam seguros, caso
+	 * contrario nao é aceito
+	 * 
+	 * Ensures that when you click on a field, the fields beside it are safe,
+	 * otherwise it is not accepted
+	 */
+
+	boolean safeneighborhood() {
+		return neighborhoods.stream().noneMatch(v -> v.mined);
 	}
 
 }
